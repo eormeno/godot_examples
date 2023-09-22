d0308c6092f11b874ad0d1b8a89d34dc110d2555
Emilio Ormeño <eormneno@gmail.com>
2023-09-21 22:37:02 -0300

    working with FEM

--- idei_game/player/player.gd
+++ idei_game/player/player.gd
@@ -3,13 +3,59 @@ extends Area2D
 @export var speed = 200
 var screen_size
 var states : Dictionary
+var states_manager : StateManager
+var velocity : Vector2 = Vector2.ZERO
 
 func _ready():
-	states = StateManager.load_states(self)
-	print(states)
+	states_manager = StateManager.new(self)
 	screen_size = get_viewport_rect().size
+	
+func start_waiting():
+	$AnimatedSprite2D.animation = "idle"
+	$AnimatedSprite2D.play()
+	
+func waiting(_delta):
+	velocity = Vector2.ZERO
+	check_input()
 
-func _process(delta):
+func start_looking_right():
+	$AnimatedSprite2D.flip_h = false
+	$AnimatedSprite2D.animation = "run"
+	$AnimatedSprite2D.play()
+	
+func looking_right(delta):
+	velocity = Vector2.ZERO
+	velocity.x += 1
+	check_input()
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
+func start_looking_left():
+	$AnimatedSprite2D.flip_h = true
+	$AnimatedSprite2D.animation = "run"
+	$AnimatedSprite2D.play()
+
+func looking_left(delta):
+	velocity = Vector2.ZERO
+	velocity.x += -1
+	check_input()
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+
+func check_input():
+	if Input.is_action_pressed("ui_right"):
+		states_manager.state_exit("move_right")
+		return
+	if Input.is_action_pressed("ui_left"):
+		states_manager.state_exit("move_left")
+		return
+	states_manager.state_exit("stop")
+
+func _processa(delta):
 	var velocity = Vector2.ZERO
 	if Input.is_action_pressed("ui_right"):
 		velocity.x += 1
--- idei_game/states_manager/states_manager.gd
+++ idei_game/states_manager/states_manager.gd
@@ -1,7 +1,21 @@
 class_name StateManager extends Node
 
-static func load_states(scene : Node, states_file_name : String = "states"):
-	var scene_path = get_scene_path(scene)
+var current : Node
+var states : Dictionary
+
+var is_changing_state : bool
+var current_state_name : String
+var current_state : Dictionary
+
+func _init(scene : Node, states_file_name : String = "states"):
+	current = scene
+	states = load_states(states_file_name)
+	current_state_name = states['start']
+	current_state = states[current_state_name]
+	current.add_child(self)
+
+func load_states(states_file_name : String):
+	var scene_path = get_scene_path()
 	if not scene_path:
 		printerr("Scene path could not be read")
 		return null
@@ -11,10 +25,18 @@ static func load_states(scene : Node, states_file_name : String = "states"):
 		return
 	var file = FileAccess.open(states_full_path, FileAccess.READ)
 	var data = JSON.parse_string(file.get_as_text())
+	reset_states_values(data)
 	return data
+	
+func reset_states_values(states_dict : Dictionary):
+	for key in states_dict.keys():
+		if key == "start":
+			continue
+		states_dict[key]['running_method'] = null
+		states_dict[key]['started'] = false
 
-static func get_scene_path(scene : Node):
-	var root_scene = scene.get_tree().get_current_scene()
+func get_scene_path():
+	var root_scene = current.get_tree().get_current_scene()
 	if not root_scene:
 		printerr("No root scene.")
 		return null
@@ -23,3 +45,67 @@ static func get_scene_path(scene : Node):
 		printerr("Scene does not have an associated file.")
 		return null
 	return root_scene_file_path.get_base_dir()
+	
+func _process(delta):
+	if not current:	return
+	if is_changing_state: return
+	
+	run_current_state(delta)
+	
+func state_exit(label_name : String):
+	var exit_state_name = current_state.get(label_name)
+	if not exit_state_name:
+		return
+	if current_state_name == exit_state_name:
+		return
+	current_state.running_method = null
+	current_state.started = false
+	run_state_end_callback()
+	current_state_name = exit_state_name
+	current_state = states[exit_state_name]
+	pass
+
+func run_current_state(delta):
+	var running_method = current_state.get("running_method")
+	
+	if running_method:
+		current.call(running_method, delta)
+		return
+		
+	is_changing_state = true
+	run_state_start_callback()
+	run_state_callback(delta)
+	is_changing_state = false
+
+func run_state_start_callback():
+	if current_state.started:
+		return
+	var callback_method_name_suffix = current_state.get("start")
+	if not callback_method_name_suffix:
+		return
+	var starting_callback_name = 'start_' + callback_method_name_suffix
+	if not current.has_method(starting_callback_name):
+		return
+	current.call(starting_callback_name)
+	current_state.started = true
+	print('started ', starting_callback_name)
+
+func run_state_callback(delta):
+	var callback_method_name_suffix = current_state.get("start")
+	if not callback_method_name_suffix:
+		return
+	if not current.has_method(callback_method_name_suffix):
+		return
+	print('calling ', callback_method_name_suffix)
+	current.call(callback_method_name_suffix, delta)
+	current_state['running_method'] = callback_method_name_suffix
+
+func run_state_end_callback():
+	var callback_method_name_suffix = current_state.get("start")
+	if not callback_method_name_suffix:
+		return
+	var end_callback_name = 'end_' + callback_method_name_suffix
+	if not current.has_method(end_callback_name):
+		return
+	current.call(end_callback_name)
+	print('called ', end_callback_name)
