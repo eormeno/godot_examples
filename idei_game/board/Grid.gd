620e49115708b5be8c75655c219e7c276eea0e2e
Emilio Ormeño <eormneno@gmail.com>
2023-09-24 13:36:59 -0300

    changes

--- idei_game/board/Grid.gd
+++ idei_game/board/Grid.gd
@@ -1,17 +1,14 @@
-extends Node2D
+extends TileMap
 
 var screen_size
 var screen_center
 var player : Node
-@export var grid_size : int = 32
-
-
+@export var grid_size : int = 64
 
 func _ready():
-	screen_size = get_viewport_rect().size
+	screen_size = calculate_bounds(self)
 	screen_center = screen_size / 2
 	player = $Player
-	player.position = screen_center
 
 func _draw():
 	draw_line(Vector2(screen_center.x, 0), Vector2(screen_center.x, screen_size.y), Color.CORAL, 0.5, true)
@@ -21,3 +18,12 @@ func _draw():
 		draw_line(Vector2(x, 0), Vector2(x, screen_size.y), Color.DIM_GRAY, 0.5, true)
 	for y in range(0, screen_size.y, grid_size):
 		draw_line(Vector2(0, y), Vector2(screen_size.x, y), Color.DIM_GRAY, 0.5, true)
+		
+func calculate_bounds(tilemap : TileMap):
+	return Vector2i(960,960)
+#	var cell_bounds = tilemap.get_used_rect()
+#	var data = tilemap.tile_set.tile_size
+#	# create transform
+#	var cell_to_pixel = Transform2D(Vector2(data.x * tilemap.scale.x, 0), Vector2(0, data.y * tilemap.scale.y), Vector2())
+#	# apply transform
+#	return Rect2(cell_to_pixel * cell_bounds.position, cell_to_pixel * cell_bounds.size)
--- idei_game/editor/editor.gd
+++ idei_game/editor/editor.gd
@@ -1,16 +1,15 @@
-extends VBoxContainer
+extends Control
 
 func _ready():
 	var new_cmds : Array[Node] = BotCommandFactory.instance().create_available()
 	for cmd in new_cmds:
-		$Commands.add_child(cmd)
-		$Commands.add_spacer(false)
+		$MarginContainer/Panel/VBoxContainer/Commands.add_child(cmd)
+		$MarginContainer/Panel/VBoxContainer/Commands.add_spacer(false)
 		cmd.connect("pressed", on_pressed)
 	#BotCommandFactory.instance().list()
 
 func on_pressed(command : String):
-	$VBoxContainer/Program.add_item(command)
-	
+	$MarginContainer/Panel/VBoxContainer/VBoxContainer/Program.add_item(command)
 	
 func _process(_delta):
 	pass
