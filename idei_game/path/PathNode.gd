3df22c41914eed080b19fff5077b75d59f17a706
DAMO\Emilio <eormeno@gmail.com>
2023-10-04 13:28:20 -0300

    colored spots

--- idei_game/path/PathNode.gd
+++ idei_game/path/PathNode.gd
@@ -3,14 +3,26 @@ class_name PathNode extends Node2D
 const BACK_COMMAND = "back"
 const TEXT_FONT_SIZE : int = 8
 @export var place_name : String = "next"
+@export var light_color : Color = Color.ALICE_BLUE
 static var font
-
+static var light_texture
+var light : PointLight2D
+var tween : Tween
 
 func _ready():
 	if not font:
 		var label : Label = Label.new()
 		font = label.get_theme_default_font()
+		light_texture = load("res://path/path1.png")
 	place_name = place_name.to_lower()
+	light = PointLight2D.new()
+	light.color = light_color
+	light.texture = light_texture
+	add_child(light)
+	tween = self.create_tween().set_loops()
+	tween.tween_property(light, "energy", 0, 1).set_trans(Tween.TRANS_LINEAR)
+	tween.tween_property(light, "energy", 1, 1).set_trans(Tween.TRANS_LINEAR)
+	tween.play()
 
 func _draw():
 #	if not Engine.is_editor_hint():
@@ -34,6 +46,8 @@ func get_destinations() -> Array[String]:
 	var ret : Array[String] = []
 	ret.append(BACK_COMMAND)
 	for trans in get_children():
+		if not trans is PathTransition:
+			continue
 		var t : PathTransition = trans
 		if not t.destination:
 			continue
