cf1af8417d84ae25c152c20741ed7fc326f119a7
Emilio Ormeño <eormneno@gmail.com>
2023-10-16 23:15:31 -0300

    integrating parser

--- aurora-api/app/Http/Controllers/ResourceController.php
+++ aurora-api/app/Http/Controllers/ResourceController.php
@@ -5,6 +5,16 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use App\Models\Resource;
 use Illuminate\Http\Request;
+use App\Parser\AdventureLexer;
+use App\Parser\AdventureParser;
+use Antlr\Antlr4\Runtime\CommonTokenStream;
+use Antlr\Antlr4\Runtime\Error\Listeners\DiagnosticErrorListener;
+use Antlr\Antlr4\Runtime\InputStream;
+use Antlr\Antlr4\Runtime\ParserRuleContext;
+use Antlr\Antlr4\Runtime\Tree\ErrorNode;
+use Antlr\Antlr4\Runtime\Tree\ParseTreeListener;
+use Antlr\Antlr4\Runtime\Tree\ParseTreeWalker;
+use Antlr\Antlr4\Runtime\Tree\TerminalNode;
 
 class ResourceController extends Controller
 {
@@ -15,9 +25,7 @@ class ResourceController extends Controller
         $folderTree = $this->getFolderTree($user->userFolder);
         return response()->json([
             'folder' => $folderTree
-        ])->withHeaders([
-            'Request-ID' => $request_id,
-        ]);
+        ])->withHeaders(['Request-ID' => $request_id,]);
     }
 
     private function getFolderTree($folder)
@@ -51,9 +59,7 @@ class ResourceController extends Controller
         $request_id = request()->header('Request-ID');
         return response()->json([
             'resource' => $resource
-        ])->withHeaders([
-            'Request-ID' => $request_id,
-        ]);
+        ])->withHeaders(['Request-ID' => $request_id,]);
     }
 
     /**
@@ -62,12 +68,26 @@ class ResourceController extends Controller
     public function update(Request $request, Resource $resource)
     {
         $request_id = request()->header('Request-ID');
+        $script = $request->input('content');
+        $code = $this->parseScript($script);
         $resource->update($request->all());
         return response()->json([
-            'resource' => $resource
-        ])->withHeaders([
-            'Request-ID' => $request_id,
-        ]);
+            'resource' => $resource,
+            'code' => $code
+        ])->withHeaders(['Request-ID' => $request_id,]);
+    }
+
+    private function parseScript(string $script)
+    {
+        $input = InputStream::fromString($script);
+        $lexer = new AdventureLexer($input);
+        $tokens = new CommonTokenStream($lexer);
+        $parser = new AdventureParser($tokens);
+        $parser->addErrorListener(new DiagnosticErrorListener());
+        $tree = $parser->program();
+        $treeListener = new TreeShapeListener();
+        ParseTreeWalker::default()->walk($treeListener, $tree);
+        return $treeListener->getGeneratedCode();
     }
 
     /**
@@ -76,5 +96,68 @@ class ResourceController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+}
+
+final class TreeShapeListener implements ParseTreeListener {
+
+    private $vars = [];
+    private $code = [];
+
+    public function __construct($code = []) {
+        $this->code = $code;
+    }
+
+    public function getGeneratedCode() {
+        return $this->code;
+    }
+
+    public function visitTerminal(TerminalNode $node) : void {
+    }
+    public function visitErrorNode(ErrorNode $node) : void {
+        $line = $node->getSymbol()->getLine();
+        $msg = $node->getSymbol()->getText();
+        echo "Error on line " . $line . ": " . $msg . "\n";
+
+    }
+    public function exitEveryRule(ParserRuleContext $ctx) : void {
+    }
+
+    public function enterEveryRule(ParserRuleContext $ctx) : void {
+        if ($ctx instanceof Context\AssignmentContext) {
+            $id = $ctx->ID()->getText();
+            $expr = $ctx->expression()->getText();
+            $this->code[] = $id . " = " . $expr;
+            $this->vars[$id] = $expr;
+        }
+        if ($ctx instanceof Context\Move_statementContext) {
+            $id_expr = $ctx->expression()->ID();
+            $str_expr = $ctx->expression()->STRING();
+            $value = null;
+            if ($id_expr != null) {
+                $id = $id_expr->getText();
+                $value = $this->vars[$id];
+            } else if ($str_expr != null) {
+                $value = $str_expr->getText();
+            } else {
+                $value = $ctx->expression()->getText();
+            }
+            $this->code[] = "move " . $value;
+        }
+
+        if ($ctx instanceof Context\Take_statementContext) {
+            $id_expr = $ctx->expression()->ID();
+            $str_expr = $ctx->expression()->STRING();
+            $value = null;
+            if ($id_expr != null) {
+                $id = $id_expr->getText();
+                $value = $this->vars[$id];
+            } else if ($str_expr != null) {
+                $value = $str_expr->getText();
+            } else {
+                $value = $ctx->expression()->getText();
+            }
+            $this->code[] = "take " . $value;
+        }
     }
 }
