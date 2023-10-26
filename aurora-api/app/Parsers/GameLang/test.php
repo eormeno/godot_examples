dd1f2d33894598770fd23d718a3a1f644783a1ee
Emilio Ormeño <eormneno@gmail.com>
2023-10-26 20:04:28 -0300

    pruebas

--- aurora-api/app/Parsers/GameLang/test.php
+++ aurora-api/app/Parsers/GameLang/test.php
@@ -38,14 +38,67 @@ class GameLangSpecificVisitor extends GameLangBaseVisitor
     {
         $identificators = $context->ID();
         foreach ($identificators as $identificator) {
-            echo "param: $identificator \n";
+            // echo "param: $identificator \n";
         }
     }
 
-    public function visitAssignment(Context\AssignmentContext $context) {
+    public function visitAssignment(Context\AssignmentContext $context)
+    {
+        $line = $context->getStart()->getLine();
         $identificator = $context->ID()->getText();
-        $expression = $context->expression()->getText();
-        echo "$identificator = $expression\n";
+        $expression = $this->visit($context->expression());
+        echo "$line:\t$identificator = $expression\n";
+    }
+
+    public function visitExpression(Context\ExpressionContext $context)
+    {
+        $res = "";
+        $identificator = $context->ID();
+        $plus = $context->PLUS();
+        $minus = $context->MINUS();
+        $multiply = $context->MULTIPLY();
+        $divide = $context->DIVIDE();
+        $leftParen = $context->LPAREN();
+        $rightParen = $context->RPAREN();
+        $integer = $context->INT();
+        $string = $context->STRING();
+        $method = $context->methodCall();
+        $attribute = $context->attributeCall();
+        $expression = $context->expression();
+        if ($identificator)
+            $res = "[" . $identificator->getText() . "]";
+        elseif ($integer)
+            $res = $integer->getText() . ".00";
+        elseif ($string)
+            $res = "\"" . $string->getText() . "\"";
+        elseif ($method)
+            $res = $this->visit($method);
+        elseif ($attribute)
+            $res = "->" . $attribute->getText();
+        elseif ($plus)
+            $res = $this->visit($context->expression(0)) . " + " . $this->visit($context->expression(1));
+        elseif ($minus)
+            $res = $this->visit($context->expression(0)) . " - " . $this->visit($context->expression(1));
+        elseif ($multiply)
+            $res = $this->visit($context->expression(0)) . " * " . $this->visit($context->expression(1));
+        elseif ($divide)
+            $res = $this->visit($context->expression(0)) . " / " . $this->visit($context->expression(1));
+        elseif ($leftParen)
+             $res = "'(" . $this->visit($context->expression(0)) . ")'";
+        elseif ($rightParen)
+            $res = "'(" . $this->visit($context->expression(0)) . ")'";
+        elseif ($expression) {
+            if (!is_array($expression)) {
+                $res = $this->visit($expression);
+            } else {
+                $res = "";
+                foreach ($expression as $expr) {
+                    $res .= $this->visit($expr) . " ";
+                }
+            }
+        }
+
+        return $res;
     }
 }
 
