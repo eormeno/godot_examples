35c6dce7488b52715b8f9cfa1be3b6e5ac53b30b
DAMO\Emilio <eormeno@gmail.com>
2023-11-02 11:00:06 -0300

    mejorando respuesta

--- aurora-api/app/Parsers/GameLang/Operation.php
+++ aurora-api/app/Parsers/GameLang/Operation.php
@@ -28,32 +28,7 @@ enum Operation implements \JsonSerializable
     case cat; // concatenate
     case jmp; // jump
 
-    public function jsonSerialize()
-    {
-        return match ($this) {
-            self::psh => 'psh',
-            self::pop => 'pop',
-            self::add => 'add',
-            self::sub => 'sub',
-            self::mul => 'mul',
-            self::div => 'div',
-            self::reg => 'reg',
-            self::mem => 'mem',
-            self::get => 'get',
-            self::lor => 'lor',
-            self::and => 'and',
-            self::grt => 'grt',
-            self::lst => 'lst',
-            self::gte => 'gte',
-            self::lte => 'lte',
-            self::eql => 'eql',
-            self::neq => 'neq',
-            self::not => 'not',
-            self::out => 'out',
-            self::inp => 'inp',
-            self::ifi => 'ifi',
-            self::cat => 'cat',
-            self::jmp => 'jmp',
-        };
+    public function jsonSerialize(){
+        return $this->name;
     }
 }
