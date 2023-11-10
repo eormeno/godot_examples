b431f33d829eabff3e78dd35080fe7e7cf5d2d27
Emilio Ormeño <eormneno@gmail.com>
2023-11-10 19:33:44 -0300

    minimos

--- aurora-api/app/Parsers/GameLang/GameLangVM.php
+++ aurora-api/app/Parsers/GameLang/GameLangVM.php
@@ -7,6 +7,8 @@ class MemoryBlock implements JsonSerializable
 {
     private static $next_id = 0;
     private $id = 0;
+
+    // todo: quitar el static
     private static $stack = [];
     private $registers = Array(4);
     private $heap = [];
@@ -27,7 +29,6 @@ class MemoryBlock implements JsonSerializable
         if ($icl != 0) {
             echo "$icl setReg($reg, $data)\n";
         }
-        // if the data is numeric, convert it to float and round it to 2 decimal places
         if (is_numeric($data)) {
             $data = round(floatval($data), 6);
         }
@@ -172,63 +173,42 @@ class GameLangVM
     private function runCode(array $code)
     {
         $this->pushCallStack();
-        // $stack = [];
-        // $regs = [3];
-        // $mem = [];
         $i = 0;
         while ($i < count($code)) {
-//        for ($i = 0; $i < count($code); $i++) {
             [$lineNumber, $op, $reg, $data] = $code[$i];
             switch ($op) {
                 case Operation::reg:
                     $this->getBlock()->setReg($reg, $data);
-                    // $regs[$reg] = $data;
                     break;
                 case Operation::psh:
                     $this->getBlock()->push($reg, $data);
-                    // array_push($stack, $regs[$reg]);
                     break;
                 case Operation::pop:
                     $this->getBlock()->pop($reg, $data);
-                    // $regs[$reg] = array_pop($stack);
                     break;
                 case Operation::add:
                     $left = $this->getBlock()->getReg(1);
                     $right = $this->getBlock()->getReg(2);
                     $this->getBlock()->setReg($reg, $left + $right);
-                    // $left = $regs[1];
-                    // $right = $regs[2];
-                    // $regs[$reg] = $left + $right;
                     break;
                 case Operation::sub:
                     $left = $this->getBlock()->getReg(1);
                     $right = $this->getBlock()->getReg(2);
                     $this->getBlock()->setReg($reg, $left - $right);
-                    // $left = $regs[1];
-                    // $right = $regs[2];
-                    // $regs[$reg] = $left - $right;
                     break;
                 case Operation::mul:
                     $left = $this->getBlock()->getReg(1);
                     $right = $this->getBlock()->getReg(2);
                     $this->getBlock()->setReg($reg, $left * $right);
-                    // $left = $regs[1];
-                    // $right = $regs[2];
-                    // $regs[$reg] = $left * $right;
                     break;
                 case Operation::div:
                     $left = $this->getBlock()->getReg(1);
                     $right = $this->getBlock()->getReg(2);
                     $this->getBlock()->setReg($reg, $left / $right);
-                    // $left = $regs[1];
-                    // $right = $regs[2];
-                    // $regs[$reg] = $left / $right;
                     break;
                 case Operation::mem:
                     $result = $this->getBlock()->getReg($reg);
                     $this->getBlock()->setHeap($data, $result);
-                    // $result = $regs[$reg];
-                    // $mem[$data] = $result;
                     break;
                 case Operation::get:
                     if (!$this->getBlock()->has($data)) {
@@ -236,81 +216,50 @@ class GameLangVM
                     }
                     $value = $this->getBlock()->getHeap($data);
                     $this->getBlock()->setReg($reg, $value);
-                    // if (!array_key_exists($data, $mem)) {
-                    //     throw new \Exception("Undefined variable '$data' at line $lineNumber.");
-                    // }
-                    // $value = $mem[$data];
-                    // $regs[$reg] = $value;
                     break;
                 case Operation::eql:
                     $left = $this->getBlock()->getReg(1);
                     $right = $this->getBlock()->getReg(2);
                     $this->getBlock()->setReg($reg, $left == $right);
-                    // $left = $regs[1];
-                    // $right = $regs[2];
-                    // $regs[$reg] = $left == $right;
                     break;
                 case Operation::neq:
                     $left = $this->getBlock()->getReg(1);
                     $right = $this->getBlock()->getReg(2);
                     $this->getBlock()->setReg($reg, $left != $right);
-                    // $left = $regs[1];
-                    // $right = $regs[2];
-                    // $regs[$reg] = $left != $right;
                     break;
                 case Operation::grt:
                     $left = $this->getBlock()->getReg(1);
                     $right = $this->getBlock()->getReg(2);
                     $this->getBlock()->setReg($reg, $left > $right);
-                    // $left = $regs[1];
-                    // $right = $regs[2];
-                    // $regs[$reg] = $left > $right;
                     break;
                 case Operation::lst:
                     $left = $this->getBlock()->getReg(1);
                     $right = $this->getBlock()->getReg(2);
                     $this->getBlock()->setReg($reg, $left < $right);
-                    // $left = $regs[1];
-                    // $right = $regs[2];
-                    // $regs[$reg] = $left < $right;
                     break;
                 case Operation::gte:
                     $left = $this->getBlock()->getReg(1);
                     $right = $this->getBlock()->getReg(2);
                     $this->getBlock()->setReg($reg, $left >= $right);
-                    // $left = $regs[1];
-                    // $right = $regs[2];
-                    // $regs[$reg] = $left >= $right;
                     break;
                 case Operation::lte:
                     $left = $this->getBlock()->getReg(1);
                     $right = $this->getBlock()->getReg(2);
                     $this->getBlock()->setReg($reg, $left <= $right);
-                    // $left = $regs[1];
-                    // $right = $regs[2];
-                    // $regs[$reg] = $left <= $right;
                     break;
                 case Operation::lor:
                     $left = $this->getBlock()->getReg(1);
                     $right = $this->getBlock()->getReg(2);
                     $this->getBlock()->setReg($reg, $left || $right);
-                    // $left = $regs[1];
-                    // $right = $regs[2];
-                    // $regs[$reg] = $left || $right;
                     break;
                 case Operation::and:
                     $left = $this->getBlock()->getReg(1);
                     $right = $this->getBlock()->getReg(2);
                     $this->getBlock()->setReg($reg, $left && $right);
-                    // $left = $regs[1];
-                    // $right = $regs[2];
-                    // $regs[$reg] = $left && $right;
                     break;
                 case Operation::not:
                     $left = $this->getBlock()->getReg(1);
                     $this->getBlock()->setReg($reg, !$left);
-                    // $left = $regs[1];
-                    // $regs[$reg] = !$left;
                     break;
                 case Operation::cat:
                     $left = $this->getBlock()->getReg(1);
@@ -322,34 +271,16 @@ class GameLangVM
                         $right = $right ? 'V' : 'F';
                     }
                     $this->getBlock()->setReg($reg, $left . $right);
-                    // $left = $regs[1];
-                    // $right = $regs[2];
-                    // if (is_bool($left)) {
-                    //     $left = $left ? 'V' : 'F';
-                    // }
-                    // if (is_bool($right)) {
-                    //     $right = $right ? 'V' : 'F';
-                    // }
-                    // $regs[$reg] = $left . $right;
                     break;
                 case Operation::out:
                     $str = $this->getBlock()->getReg($reg);
-                    // $str = $regs[$reg];
                     echo $str . "\n";
                     break;
                 case Operation::ifi:
-                    // dd($regs[0], $regs[1], $i, count($code));
                     $condition = $this->getBlock()->getReg(0);
-                    // $condition = $regs[0];
                     if (!$condition) {
-                        // dd($i, $regs[1]);
                         $i = $data - 1; // -1 because the loop will increment it
                     }
-                    // $condition = $regs[0];
-                    // if (!$condition) {
-                    //     // dd($i, $regs[1]);
-                    //     $i = $data;
-                    // }
                     break;
                 case Operation::jmp:
                     $jump_to = $data;
@@ -368,7 +299,6 @@ class GameLangVM
                     $i = $data - 1; // -1 because the loop will increment it
                     break;
                 case Operation::delta:
-                    // $regs[$reg] = 0.01; // simulates a small delta time
                     $this->getBlock()->setReg($reg, 0.2);
                     break;
                 case Operation::end:
@@ -377,7 +307,5 @@ class GameLangVM
             }
             $i++;
         }
-        // echo json_encode($mem, JSON_PRETTY_PRINT) . "\n";
     }
-
 }
