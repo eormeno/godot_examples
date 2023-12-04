68890014effbc9bc0d9c639c247aab53d29362a2
Emilio Ormeño <eormneno@gmail.com>
2023-12-04 01:23:21 -0300

    remove images script

---
+++ aurora-docker/remove-images.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+    echo "Docker is not installed. Please install Docker and try again."
+    exit 1
+fi
+
+# Check if Docker daemon is running
+if ! sudo systemctl is-active --quiet docker; then
+  echo "Docker daemon is not running."
+fi
+
+# Stop containers
+sudo docker stop $(docker ps -a -q) 1>/dev/null
+
+# Remove containers
+sudo docker rm $(docker ps -a -q) 1>/dev/null
+
+# Remove images
+sudo docker rmi $(docker images -a -q) 1>/dev/null
