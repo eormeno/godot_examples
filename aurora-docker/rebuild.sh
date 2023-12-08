d42d274ab1774d7f28a175b4b152a3b121a29a82
Emilio Ormeño <eormneno@gmail.com>
2023-12-08 00:06:59 -0300

    rebuild

---
+++ aurora-docker/rebuild.sh
@@ -0,0 +1,8 @@
+./remove-containers.sh
+./remove-images.sh
+sudo docker compose -f docker-compose-prod.yml up -d --build app
+sudo docker compose run --rm composer update
+sudo docker compose run --rm artisan migrate
+sudo docker compose run --rm artisan db:seed
+sudo docker compose run --rm artisan key:generate
+sudo docker compose run --rm artisan users
